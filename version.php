6e8895e6052844a6036957eb9b8aab7cbeccb3ac
Jonatha Carvalho <80285848+jowgaze@users.noreply.github.com>
2025-09-28 19:39:22 -0300

    test in moodle 4.5.6

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_chatbot_ai';
-$plugin->version   = 2025090800;
-$plugin->requires  = 2025041402;
+$plugin->version   = 2025092800;
+$plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = '1.0';
