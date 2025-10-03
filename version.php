594b73a47052d9557e0686e540e6017cf4604bf6
jowgaze <jonatha.biomon.hp@gmail.com>
2025-10-02 21:15:46 -0300

    update version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_chatbot_ai';
-$plugin->version   = 2025092800;
+$plugin->version   = 2025100200;
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0';
+$plugin->release   = '1.1';
